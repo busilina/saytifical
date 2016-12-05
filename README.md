549dd962557399695f303f89382b9eba4c6ae40d
muhanerd <muhannad.ali@live.de>
2016-12-05 23:29:08 +0100

    setup

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # saytifical
 say it scientifically
+
+# How to
+- Start server/db: mongod --dbpath data/
+    - open http://localhost:27017/
+- If the server is already running:
+    - ps wuax | grep mongo
+    - sudo kill <PID>
+- Delete db: mongo <dbname> --eval "db.dropDatabase()"
+- Create db (e.g., saytifical):
+    - mongo
+    - use saytifical
+    - db.saytifica.insert([{
+          "name" : "Priming effect",
+          "definition" : "Priming is an implicit memory effect in which exposure to one stimulus influences a response to another stimulus.",
+          "aka" : "Context priming",
+          "video" : "HRAKt0GakJM",
+          "category" : "Psychology",
+          "icon" : "priming_effect.jpg"
+        }])
+    - db.saytifica.find().pretty()
+- npm install && npm start
+    - http://localhost:3000/saytifica
---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var mongodb = require('mongodb');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/saytifica', function(req, res, next) {
+  var MongoClient = mongodb.MongoClient;
+  var url = 'mongodb://localhost:27017/saytifical';
+  MongoClient.connect(url, function(err, db){
+    if(err){
+      console.log('Unable to connect to the server', err)
+    }else{
+      console.log('Connection established.');
+      var collection = db.collection('saytifica');
+      collection.find({}).toArray(function(err, result){
+        if(err){
+          res.send(err);
+        }else if(result.length){
+          res.render('saytifica', {
+            saytifica: result
+          })
+        }else{
+          res.send('No documents found!')
+        }
+      })
+      db.close();
+    }
+  })
+});
+
+module.exports = router;
